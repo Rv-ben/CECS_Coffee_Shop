f1af8653dd788ef2d48f8f2d07b3bf8cdec37479
Rv-ben <Rubenbramasco@gmail.com>
2019-11-30 01:00:08 -0800

    Added params to create product

--- Factories/Factory.java
+++ Factories/Factory.java
@@ -2,8 +2,18 @@ package Factories;
 
 import Products.Product;
 
+/**
+ * Factory interface
+ * @author Ruben Bramasco
+ * @version 1.0.0
+ * @since 11-30-2019
+ */
 public interface Factory{
 
-    public abstract Product createProduct();
+    /**
+     * Creates a product
+     * @return Product Object
+     */
+    public abstract Product createProduct(Object details ,Object Decorators);
 
 }
