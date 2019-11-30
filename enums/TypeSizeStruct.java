e2fde4f8d47956617c2d4b473d604a53a22d765e
Rv-ben <Rubenbramasco@gmail.com>
2019-11-30 01:01:21 -0800

    Struct to handle details of size and type

---
+++ enums/TypeSizeStruct.java
@@ -0,0 +1,12 @@
+package enums;
+
+/**
+ * Struct class holding size and type of drink
+ * @author Ruben Bramasco
+ * @version 1.0.0
+ * @since 11-30-2019
+ */
+public class TypeSizeStruct{
+    public Sizes size;
+    public DrinkTypes type;
+}
