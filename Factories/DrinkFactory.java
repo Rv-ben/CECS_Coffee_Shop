86c2fd9a1c0d0a149d0bcfe4b0250b16ff08e1d1
Rv-ben <rubenbramasco@gmail.com>
2019-11-29 22:41:02 -0800

    Added  a drink factory, needs a cast check

---
+++ Factories/DrinkFactory.java
@@ -0,0 +1,46 @@
+package Factories;
+
+import Products.Product;
+
+import java.util.ArrayList;
+
+import Decorators.Milk;
+import enums.*;
+import Products.Coffee;
+import Products.Drink;
+
+public class DrinkFactory extends Factory{
+
+
+    @Override
+    public Product createProduct() {
+        return null;
+    }
+
+    public Product createProduct(Object type, Object decorators,Object size) {
+        Drink drink;
+
+        drink = drinkType((DrinkTypes)type, (Sizes)size);
+
+        
+        for(ToppingTypes i: (ArrayList<ToppingTypes>)decorators){
+            drink = addDecorators(i, drink);
+        }
+
+        return drink;
+    }
+
+    public Drink drinkType(DrinkTypes type,Sizes size){
+        switch(type){
+            case coffee: return new Coffee(size);
+        }
+        return null;
+    }
+
+    public Drink addDecorators(ToppingTypes toppings,Drink drink){
+        switch(toppings){
+            case milk: return new Milk(drink);
+        }
+        return drink;
+    }
+}
