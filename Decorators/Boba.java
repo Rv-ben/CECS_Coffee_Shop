058f2912ec8f748229f4bb82dd7119ce9994117b
TanSRicky <35533691+TanSRicky@users.noreply.github.com>
2019-11-30 12:29:27 -0800

    Bobas with pricing and descrips

---
+++ Decorators/Boba.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Decorators;
+
+import Products.Drink;
+
+/**
+ *
+ * @author tansr
+ */
+public class Boba extends Topping {
+    
+    public Boba(Drink base){
+        super(base);
+    }
+
+    @Override
+    public String getDescription() {
+        return base.getDescription() + "Added: Boba";
+    }
+
+    @Override
+    public double getCost() {
+        return base.getCost() + .50;
+    }
+}
