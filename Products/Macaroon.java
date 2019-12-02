70dc094293c0f3a1facd0b33704003169aeca8be
Tristen Seng <tristenseng@gmail.com>
2019-12-01 19:42:48 -0800

    added a macaroon class

---
+++ Products/Macaroon.java
@@ -0,0 +1,24 @@
+package Products;
+import enums.PastryTypes;
+
+public class Macaroon extends Pastry{
+    
+    PastryTypes spec;
+    
+    public Macaroon(PastryTypes general, PastryTypes spec){
+        super("Macaroon", general);
+        this.spec = spec;
+    }
+
+    public double getCost(PastryTypes spec){
+        double cost = super.getCost();
+        switch(spec) {
+            case variety:
+                break;
+        }
+        return cost;
+    }
+    public String getDescription(){
+       return spec.name();
+    }
+}
