afa326c306ecb311b9a0276c5373b099a8c77310
Tristen Seng <tristenseng@gmail.com>
2019-12-01 19:42:59 -0800

    added an abstract pastry class.

---
+++ Products/Pastry.java
@@ -0,0 +1,37 @@
+package Products;
+import enums.PastryTypes;
+
+public abstract class Pastry implements Product {
+    
+    String name;
+
+    PastryTypes general;
+
+    public Pastry(){
+
+    }
+
+    public Pastry(String name, PastryTypes general){
+        this.name = name;
+        this.general = general;
+    }
+
+    public double getCost() {
+        double cost = 0;
+        switch (general) {
+           case croissant:
+                cost += 2;
+                break;
+            case cookie:
+                cost += 1.00;
+                break;
+            case macaroon:
+                cost += 2.00;
+                break;
+        }
+        return cost;
+    }
+
+    
+
+}
