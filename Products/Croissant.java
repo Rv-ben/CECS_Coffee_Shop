93cc26eba5242367c521a431777458c00c817870
Tristen Seng <tristenseng@gmail.com>
2019-12-01 19:42:38 -0800

    added a croissant class

---
+++ Products/Croissant.java
@@ -0,0 +1,27 @@
+package Products;
+import enums.PastryTypes;
+
+public class Croissant extends Pastry{
+    
+    PastryTypes spec;
+    
+    public Croissant(PastryTypes general, PastryTypes spec){
+        super("Croissant", general);
+        this.spec = spec;
+    }
+
+    public double getCost(PastryTypes spec){
+        double cost = super.getCost();
+        switch(spec) {
+            case chocolateNut:
+                cost += 1.00;
+                break;
+            case plain:
+                break;
+        }
+        return cost;
+    }
+    public String getDescription(){
+       return spec.name();
+    }
+}
