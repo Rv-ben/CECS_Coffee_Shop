63f276200f1ba8df7cc35975daa13967bbde7035
Vanessa Montiel <vmontielmg@gmail.com>
2019-12-02 22:26:14 -0800

    implement createProduct methods

---
+++ Factories/PastryFactory.java
@@ -0,0 +1,68 @@
+
+
+import java.util.ArrayList;
+
+import Decorators.*;
+import enums.*;
+import Products.*;
+
+/**
+ * Pastry Factory class, implements Factory
+ * @author Vanessa Montiel
+ * @version 1.0.0
+ * @since 11-30-2019
+ */
+public class PastryFactory implements Factory{
+
+    /**
+     * Creates Pastry product given details of size/type and decoratorators
+     * @param pastryType PastryTypes Object
+     * @param quantity int 
+     * @return Product Object 
+     */
+    public Product createProduct(Object pastryType, Object spec) {
+        Pastry pastry = null;
+        pastry = getPastry(pastryType, spec);
+        
+        return pastry;
+    }
+
+    /**
+     * overloaded method that creates a Croissant
+     * @param pastryType
+     * @param spec
+     * @param heated
+     * @return 
+     */
+    public Product createProduct(Object pastryType, Object spec, boolean heated) {
+        PastryTypes specific;
+        switch(spec) {
+            case PastryTypes.chocolateNut:
+                specific = chocolateNut;
+            case PastryTypes.plain:
+                specific = plain;            
+        }
+
+        if (heated) {
+            return new Croissant(PastryTypes.croissant, specific, true);
+        }
+        return new Croissant(PastryTypes.croissant, specific, false);
+    }
+
+    /**
+     * creates a pastry object given type and specific type
+     * @param pastryType
+     * @param spec
+     * @return
+     */
+    public Product getPastry(Object pastryType, Object spec) {
+        switch (pastryType) {
+        case cookie:
+            if (spec == PastryTypes.oatmeal)
+                return new Cookie(PastryTypes.cookie, PastryTypes.oatmeal);
+            return new Cookie(PastryTypes.cookie, PastryTypes.plain);
+        case macaroon:
+            return new Macaroon(PastryTypes.macaroon, PastryTypes.variety);
+        }
+    }
+}
