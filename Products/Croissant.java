91cda572d75825befaefb6b0056aba0fe0608f63
Vanessa Montiel <vmontielmg@gmail.com>
2019-12-02 16:55:09 -0800

    added attribute and updated getCost

--- Products/Croissant.java
+++ Products/Croissant.java
@@ -2,23 +2,26 @@ package Products;
 import enums.PastryTypes;
 
 public class Croissant extends Pastry{
-    
+    boolean heated;
     PastryTypes spec;
     
-    public Croissant(PastryTypes general, PastryTypes spec){
+    public Croissant(PastryTypes general, PastryTypes spec, boolean heated){
         super("Croissant", general);
         this.spec = spec;
+        this.heated = heated;
     }
 
     public double getCost(PastryTypes spec){
         double cost = super.getCost();
+        if (heated == true)
+            cost += 0.50;
         switch(spec) {
-            case chocolateNut:
-                cost += 1.00;
-                break;
-            case plain:
-                break;
-        }
+               case chocolateNut:
+                    cost += 1.00;
+                    break;
+               case plain:
+                   break;
+            }
         return cost;
     }
     public String getDescription(){
