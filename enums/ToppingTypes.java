40a4c979d3f5a0b80df48293f49e28acc0a4745d
TanSRicky <35533691+TanSRicky@users.noreply.github.com>
2019-12-04 14:28:45 -0800

    Update ToppingTypes.java

--- enums/ToppingTypes.java
+++ enums/ToppingTypes.java
@@ -1,5 +1,7 @@
 package enums;
-
+ /**
+  * Topping types
+  */
 public enum ToppingTypes{
     halfHalf,soyMilk,boba,poppingBoba,passionFruitJelly,lycheeJelly,
     grassJelly,coconutJelly,whipCream, freshStrawberry
