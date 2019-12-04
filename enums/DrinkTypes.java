20dfe7f0ef35a1d2a04469011e36fd2bb7e4e999
TanSRicky <35533691+TanSRicky@users.noreply.github.com>
2019-12-04 14:29:26 -0800

    Update DrinkTypes.java

--- enums/DrinkTypes.java
+++ enums/DrinkTypes.java
@@ -1,5 +1,7 @@
 package enums;
-
+ /**
+  * Drink types for drink factory
+  */
 public enum DrinkTypes{
     coffee, tea, almondLatte, darkRoast, hazelnutLatte, pumpkinSpiceLatte, milkTea, jasmineGreenTea, summerMintTea;
 }
