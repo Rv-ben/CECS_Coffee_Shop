ad18267ab37d7628fb8098a20d903b7369115bd3
TanSRicky <35533691+TanSRicky@users.noreply.github.com>
2019-12-04 14:24:29 -0800

    Update Product.java

--- Products/Product.java
+++ Products/Product.java
@@ -1,9 +1,17 @@
 package Products;
-
+  /** Product - represents products that we will add toppings to
+  * 
+  */
 public interface Product{
-
+  /**
+  * getDescription
+  * @return a string with all the topping descriptions appended
+  */
     public abstract String getDescription();
-
+  /**
+  * getCost
+  * @return a double that contains the current cost of this product plus the price of toppings if any.
+  */
     public abstract double getCost();
     
 
