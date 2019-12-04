9fa615e16afcda2363af2cf4dce4947a8ba80695
TanSRicky <35533691+TanSRicky@users.noreply.github.com>
2019-12-04 14:11:40 -0800

    Update Topping.java

--- Decorators/Topping.java
+++ Decorators/Topping.java
@@ -1,15 +1,26 @@
 package Decorators;
 
 import Products.Drink;
-
+/** Represens topping
+ * @version 1.0
+ * @author tansr
+ */
 public abstract class Topping extends Drink{
 
     protected Drink base;
 
+  /** Topping constructor
+  * @param base takes base to add topping to 
+  * 
+  */
     Topping(Drink base){
         this.base = base;
     }
-
+    
+  /** getBase
+  * returns a reference to the base that this topping is added to 
+  * 
+  */
     public Drink getBase(){
         return base;
     }
