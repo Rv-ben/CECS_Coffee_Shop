f0d51c1272efbfe029b760d56b940d0b03afefe1
unknown <Rubenbramasco@gmail.com>
2019-12-08 23:12:56 -0800

    Changed file structure to work with eclipse

---
+++ src/Decorators/HalfHalf.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Decorators;
+
+import Products.Drink;
+
+/**
+ *
+ * @author tansr
+ */
+public class HalfHalf extends Topping {
+     public HalfHalf(Drink base){
+        super(base);
+    }
+
+    @Override
+    public String getDescription() {
+        return base.getDescription() + "Added: Half and Half\n";
+    }
+
+    @Override
+    public double getCost() {
+        return base.getCost() + .25;
+    }
+}
